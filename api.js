bac1fa5a7aa19ed05c7f221568e978bda3724250
Toja_Ferreira <ajferreira@ua.pt>
2022-03-26 01:40:23 +0000

    Update api and dbFunctions files

--- api.js
+++ api.js
@@ -1,22 +1,22 @@
-// Import utilities
+/* Import utilities */
 const morgan = require('morgan');
 const express = require('express');
 const mysql = require('mysql');
 
-// Import student created functions
+/* Import student created functions and environment variables */
 const dotenv = require('dotenv').config();
 const dbF = require('./dbFunctions.js');
 
-// Create app
+/* Create app */
 var app = express();
 
-// Middleware
+/* Middleware */
 app
     .use(morgan('dev'))
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
 
-// Listen to requests
+/* Listen to requests */
 const port = process.env.port || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -26,14 +26,29 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
              API ENDPOINTS                
 *******************************************
 */
+app.post('/selectClient', (req, res) => {
+    dbF.selectClient(req.body.email, process.env.DB_ENCRYPTKEY).then((result) => {
+        if(result == 1){
+            res.json({code:1, msg: "Ocorreu um erro. Por favor tente mais tarde"}) /* code 1 --> Database error */
+        }
+        else if(result == 2){
+            res.json({code:2}) /* code 2 --> User does not exist or encrypt key is incorrect */
+        }
+        else{
+            var u_mail = result[0][0]["mail"];
+            var u_fname = result[0][0]["fName"];
+            var u_lname = result[0][0]["lName"];
+            res.json({code: 0, msg:"Uma conta já existe com este email", mail: u_mail, fname: u_fname, lname: u_lname}); /* code 0 --> No errors, return user data */
+        } 
+    });
+});
 
 app.post('/createClient', (req, res) => {  
-    dbF.createClient(req.body.email, req.body.fname, req.body.lname, process.env.DB_ENCRYPTKEY)
-    .then((data) => {
-        if(data == 1){
+    dbF.createClient(req.body.email, req.body.fname, req.body.lname, req.body.birthdate, req.body.sex, req.body.street, req.body.postCode, req.body.city, req.body.country, req.body.nif, process.env.DB_ENCRYPTKEY).then((result) => {
+        if(result == 1){
             res.json({code:"Já existe uma conta com este email"}) // code 1 --> Email already exists
         }
-        else if(data == 2){
+        else if(result == 2){
             res.json({code:"Ocorreu um erro. Por favor tente mais tarde"}) // code 2 --> Database error
         }
         else{
@@ -42,33 +57,12 @@ app.post('/createClient', (req, res) => {
     });
 });
 
-
-app.post('/selectClient', (req, res) => {
-    dbF.selectClient(req.body.email, process.env.DB_ENCRYPTKEY)
-    .then((data) => {
-        if(data == 1){
-            res.json({code:1}) // code 1 --> Database error
-        }
-        else if(data == 2){
-            res.json({code:2}) // code 2 --> User does not exist or encrypt key is incorrect 
-        }
-        else{
-            var u_mail = data[0][0]["mail"];
-            var u_fname = data[0][0]["fName"];
-            var u_lname = data[0][0]["lName"];
-            res.json({code: 0, mail: u_mail, fname: u_fname, lname: u_lname}); // code 0 --> No errors, return user data
-        } 
-    });
-});
-
-
 app.post('/deleteClient', (req, res) => {
-    dbF.deleteClient(req.body.email, process.env.DB_ENCRYPTKEY)
-    .then((data) => {
-        if(data == 1){
+    dbF.deleteClient(req.body.email, process.env.DB_ENCRYPTKEY).then((result) => {
+        if(result == 1){
             res.json({code:1}) // code 1 --> Database error
         }
-        else if(data == 2){
+        else if(result == 2){
             res.json({code:2}) // code 2 --> User does not exist or encrypt key is incorrect
         }
         else{
@@ -79,7 +73,7 @@ app.post('/deleteClient', (req, res) => {
 
 
 app.post('/addClientInfo', (req, res) => {
-    dbF.addClientInfo(req.body.email, req.body.age, req.body.height, req.body.weight, req.body.fitness, req.body.pathologies, process.env.DB_ENCRYPTKEY)
+    dbF.addClientInfo(req.body.email, req.body.height, req.body.weight, req.body.fitness, req.body.bmi, req.body.pathologies, process.env.DB_ENCRYPTKEY)
     .then((data) => {
         if(data == 1){
             res.json({code:"Ocorreu um erro. Por favor tente mais tarde"}) // code 1 --> Database error
--- dbFunctions.js
+++ dbFunctions.js
@@ -1,16 +1,16 @@
-// Import utilities
+/* Import utilities */
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    createClient,
     selectClient,
+    createClient,
     deleteClient,
     addClientInfo,
     selectClientInfo
 }
 
-// Connect to database (MUST USE LEGACY AUTHENTICATION METHOD (RETAIN MYSQL 5.X COMPATIBILITY))
+/* Connect to database (MUST USE LEGACY AUTHENTICATION METHOD (RETAIN MYSQL 5.X COMPATIBILITY)) */
 const dbconnection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -19,7 +19,6 @@ const dbconnection = mysql.createConnection({
 });
 
 dbconnection.connect((error) => {
-
     if (error) {
         console.log("ERROR: Problems connecting to database!");
     }
@@ -31,17 +30,18 @@ dbconnection.connect((error) => {
             STORED PROCEDURES               
 *******************************************
 */
-function createClient(mail, fName, lName, userKey) {
+function selectClient(mail, userKey) {
     return new Promise((resolve) => {
 
-        var sql = 'CALL spCreateClient(?,?,?,?)';
+        var sql = 'CALL spSelectClient(?,?)';
 
-        dbconnection.query(sql, [mail, fName, lName, userKey], (err, data) => {
-            if (err && err.errno==1062) {
+        dbconnection.query(sql, [mail, userKey], (err, data) => {
+            
+            if (err) {
                 resolve(1);
             }
-            else if (typeof data !== 'undefined' && data["affectedRows"] == 1) { 
-                resolve(0);
+            else if (typeof data !== 'undefined' && data.length > 0 && data[0].length > 0) {
+                resolve(data);
             }
             else {
                 resolve(2);
@@ -50,17 +50,17 @@ function createClient(mail, fName, lName, userKey) {
     });
 };
 
-function selectClient(mail, userKey) {
+function createClient(mail, fName, lName, birthdate, sex, street, postCode, city, country, nif, userKey) {
     return new Promise((resolve) => {
 
-        var sql = 'CALL spSelectClient(?,?)';
+        var sql = 'CALL spCreateClient(?,?,?,?,?,?,?,?,?,?,?)';
 
-        dbconnection.query(sql, [mail, userKey], (err, data) => {
-            if (err) {
+        dbconnection.query(sql, [mail, fName, lName, birthdate, sex, street, postCode, city, country, nif, userKey], (err, data) => {
+            if (err && err.errno==1062) {
                 resolve(1);
             }
-            else if (typeof data !== 'undefined' && data.length > 0 && data[0].length > 0) {
-                resolve(data);
+            else if (typeof data !== 'undefined' && data["affectedRows"] == 0) { 
+                resolve(0);
             }
             else {
                 resolve(2);
@@ -78,7 +78,7 @@ function deleteClient(mail, userKey) {
             if (err) {
                 resolve(1);
             }
-            else if (typeof data !== 'undefined' && data["affectedRows"] == 1) {
+            else if (typeof data !== 'undefined' && data["affectedRows"] == 0) {
                 resolve(0);
             }
             else {
@@ -88,16 +88,15 @@ function deleteClient(mail, userKey) {
     });
 };
 
-function addClientInfo(mail, age, height, weight, fitness, pathologies, userKey) {
+function addClientInfo(mail, height, weight, fitness, bmi, pathologies, userKey) {
     return new Promise((resolve) => {
 
         var sql = 'CALL spAddClientInfo(?,?,?,?,?,?,?)';
-
-        dbconnection.query(sql, [mail, age, height, weight, fitness, pathologies, userKey], (err, data) => {
+        dbconnection.query(sql, [mail, height, weight, fitness, bmi, pathologies, userKey], (err, data) => {
             if (err) {
                 resolve(1);
             }
-            else if (typeof data !== 'undefined' && data["affectedRows"] == 1) {
+            else if (typeof data !== 'undefined' && data["affectedRows"] == 0) {
                 resolve(0);
             }
             else {
